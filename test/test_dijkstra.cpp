db8e05488fb3700fb238e6028c07d366b9828aa9
Jandeman616 <114077286+Jandeman616@users.noreply.github.com>
2025-08-17 16:32:16 +0200

    Made a slightly complexer test for dijkstra

--- test/test_dijkstra.cpp
+++ test/test_dijkstra.cpp
@@ -43,3 +43,58 @@ TEST_CASE("Dijkstra finds shortest path and weight", "[graph]") {
 	delete b;
 	delete c;
 }
+
+TEST_CASE("Dijkstra handles cycles and multiple shortest paths", "[graph][complex]") {
+	// Graph: A->B:1, B->C:2, C->A:1, A->D:2, D->C:1, B->D:2
+	Node* a = new Node("A");
+	Node* b = new Node("B");
+	Node* c = new Node("C");
+	Node* d = new Node("D");
+
+	Edge* ab = new Edge(a, b, 1);
+	Edge* bc = new Edge(b, c, 2);
+	Edge* ca = new Edge(c, a, 1);
+	Edge* ad = new Edge(a, d, 2);
+	Edge* dc = new Edge(d, c, 1);
+	Edge* bd = new Edge(b, d, 2);
+
+	a->edges.push_back(ab);
+	a->edges.push_back(ad);
+	b->edges.push_back(bc);
+	b->edges.push_back(bd);
+	c->edges.push_back(ca);
+	d->edges.push_back(dc);
+
+	std::vector<Node*> nodes = {a, b, c, d};
+	std::vector<Edge*> edges = {ab, bc, ca, ad, dc, bd};
+	Graph graph(nodes, edges);
+	Dijkstra dijkstra(graph);
+
+	// Shortest path from A to C: A->B->C (cost 3) or A->D->C (cost 3)
+	std::vector<Node*> path = dijkstra.findShortestPath(a, c);
+	REQUIRE(path.size() == 3);
+	REQUIRE(path[0]->label == "A");
+	REQUIRE(path[2]->label == "C");
+	int weight = graph.getCostOfPath(path);
+	REQUIRE(weight == 3);
+
+	// Shortest path from C to D: C->A->D (cost 3) or C->A->B->D (cost 4)
+	std::vector<Node*> path_cd = dijkstra.findShortestPath(c, d);
+	REQUIRE(path_cd.size() == 3);
+	REQUIRE(path_cd[0]->label == "C");
+	REQUIRE(path_cd[2]->label == "D");
+	int weight_cd = graph.getCostOfPath(path_cd);
+	REQUIRE(weight_cd == 3);
+
+	// Clean up
+	delete ab;
+	delete bc;
+	delete ca;
+	delete ad;
+	delete dc;
+	delete bd;
+	delete a;
+	delete b;
+	delete c;
+	delete d;
+}
